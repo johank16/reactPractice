9beb2ad6d0987eaff03cabf193c49bba8729c842
Matteo Kruijssen <jmkruijssen@Matteos-MacBook-Pro-2.local>
2020-11-09 21:56:41 -0500

    changing ports

--- online chat app/src/server/index.js
+++ online chat app/src/server/index.js
@@ -81,7 +81,7 @@ chatWS.on('connection', ws => {
 
 
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+app.listen(process.env.PORT || 5006, () => console.log(`Listening on port ${process.env.PORT || 5006}!`));
 
 
 
